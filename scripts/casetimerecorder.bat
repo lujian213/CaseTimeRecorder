d3c2fd081f8540c3a95525bfff4106bcc100935f
lujian213 <lujian213@msn.com>
2025-08-31 16:14:19 +0800

    fix: update application title in launcher scripts to CaseTimeRecorder

--- scripts/casetimerecorder.bat
+++ scripts/casetimerecorder.bat
@@ -3,4 +3,4 @@ cd %~dp0 ..
 set LIB_DIR=.\lib
 set CONF_DIR=.\conf
 set EXPORT_DIR=.\export
-"%JAVA_HOME%\bin\java.exe" -Xmx1024m -classpath %LIB_DIR%\*;%CONF_DIR%;%EXPORT_DIR% -Dtitle=EggFund org.springframework.boot.loader.launch.JarLauncher
+"%JAVA_HOME%\bin\java.exe" -Xmx1024m -classpath %LIB_DIR%\*;%CONF_DIR%;%EXPORT_DIR% -Dtitle=CaseTimeRecorder org.springframework.boot.loader.launch.JarLauncher
